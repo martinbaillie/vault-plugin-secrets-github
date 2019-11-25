3817d3df05209b732a0f1b0a7f023cc79c6e88d0
Martin Baillie <mbaillie@tyro.com>
2019-11-26 08:21:10 +1100

    Remove old test IDs and keys

--- github/backend_test.go
+++ github/backend_test.go
@@ -14,40 +14,6 @@ import (
 	is "gotest.tools/assert/cmp"
 )
 
-const (
-	_martinVaultAppID        = "45792"
-	_martinVaultInstID       = "5018413"
-	_martinVaultClientID     = "Iv1.18ee8be3a4d623c2"
-	_martinVaultClientSecret = "30ca9eb3e81bdfeeafbc4962ae7aba5ab10484e0"
-	_martinVaultPrvKey       = `-----BEGIN RSA PRIVATE KEY-----
-MIIEpAIBAAKCAQEAvPdtXHe2CjNgCZbjpPGT9tkOloOvEfrqQhW5uGq2vRepdMWE
-rrV0CtcIk6kYRPfW9D8+XZfI7YBnW2uy/CqTUVWqVfnz70uOzurmOAeYOVzwQtHB
-e4GXrdVZR2pKD1De2hO5o1QL09Er99EMxaD9mPFsWqj0ybrYYRPeT0b0kgmdwgu7
-QRJjUW6Ynbb6GCYI3VcRkxaEE3Hd9BQhDyLpXAnyStM1h5hnxDsx5hGnbadWyII0
-ztLQjFlNDkv/5gvaPTgyq7VI5lKy5nCoIuAhKfAMim6Wv6XkLO0eamXDSzleCAkV
-A4X14fwFB1lTh07h/rITsqnLlzmSivov243lXwIDAQABAoIBAAIsjpOLxQplAOvb
-Uo8pQFyMrsBIj5ETY/KSsWpPv/548TgotZgU+lrNkomwXlkcyNpSh/MnteYVnhrN
-KGgCTWOYKreGgqn0IpZ62V83pyaxzQnXw/QZz5buZ3KM5IE8mvPDWlVouCIdu/XD
-7/OuIHzL+kUowHg5sBed1ObwmAu+D4ASld+Hey0cz4Vo2am3jIYigSju/9P5rpg0
-m0tzIzDHkTRVS2j/2mxROPSDMtx79bH7XyqP7bWwP5ohAIHLuBCmNDotAlrwS5/S
-5uHwuUHapK5UnhSk0E1Ljzj6Uj1Y16OxodsW0wWvckiFZyVCTqzVWDUfAGbY8Yy/
-hv05BsECgYEA7+71Brxeoam3KY4qF9zGzFi6l0ovqXboXk7JZ/IgquYb8xrNC99h
-K4wOts8eYYQInJGFv2nowjqqMh1R6ALKPudf0QLNvXiQjmP/hViXoYLLWhQZcTqh
-f3XIFrFG4lKZaFfbbUTB8c4hqjyhaGALzZdIOecguc5Eekgd8rHT+FECgYEAyZ7F
-6s2UR+eNHi37NDQrjBLcjCF2jYxoCCwDl9IqxPCUXkmoiqklGNnoJPDfHUcMq/mC
-hQjfGVBXEfxQy5BWBn/azNHoCxtffWyIqaJabR9eO/pON2zyaso0ePD1Cy9CmHoa
-HadyfiH0K0UNl2f1Awpgq3Y+iVkjIo0wh5pxRq8CgYEAlN5oC4jfEmFyMwdxWKL0
-eh/nji1Ki1Qq8zDhSGx0FnV2DA2qAd7UtKdPDeBO0mSQz0x/dveoorKxnSySAGmS
-wRrgWZfqvc/LqX2kMkF4u46iy75C1v1w7NvQjTvrZkunwZUZoZ+S0ox6WN0LrO5D
-BIoEF2Ev7flshY6vfkEV6zECgYBDqojMHADW/Qxsg4waYiP3V+EzGov6R5Qmofb2
-vi9id0ekOV5aYxgzNfbmZvzKi6ziDtRSJ78QdNk0DRVkrGViuwhI023oRGCQ7JsV
-K4rjsrJiuMgFbAgT9RcVO/FRtqPIHrqjFy6izGxxFTTRiq89PP9irEWivQrArtgJ
-urinuwKBgQCnXeqv8R4Rc3AVYK4PPGhkJHdO603HLwyWKjZaTT5voHUr19gQs9uW
-WW+XkxRkmQ3GiAFzrwM/aI/86E2+ZNQNHvKQ+3MYh0/npuJgumszNWlmwWfxZV5o
-VLzTE2sMc4ivGY2f9WcCvOIVXwYnKbDOTDJ0GicdWPljnRsZSNBxKQ==
------END RSA PRIVATE KEY-----`
-)
-
 func TestFactory(t *testing.T) {
 	t.Parallel()
 
