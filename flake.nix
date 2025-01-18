1c11fa8cccdbf5a8463c7f2e63102d3f2cb36155
Martin Baillie <martin@baillie.id>
2025-01-18 20:13:24 +1100

    Bump project toolchain and deps

--- flake.nix
+++ flake.nix
@@ -2,33 +2,34 @@
   description = "vault-plugin-secrets-github";
 
   inputs = {
-    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
-    flake-parts.url = github:hercules-ci/flake-parts;
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
     devshell = {
-      url = github:numtide/devshell;
+      url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     gomod2nix = {
-      url = github:nix-community/gomod2nix;
+      url = "github:nix-community/gomod2nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     gitignore = {
-      url = github:hercules-ci/gitignore.nix;
+      url = "github:hercules-ci/gitignore.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs =
-    inputs@{ self
-    , nixpkgs
-    , flake-parts
-    , devshell
-    , gomod2nix
-    , gitignore
-    , ...
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      devshell,
+      gomod2nix,
+      gitignore,
+      ...
     }:
     flake-parts.lib.mkFlake { inherit inputs; } ({
       systems = [
@@ -38,7 +39,13 @@
         "aarch64-linux"
       ];
 
-      perSystem = { config, pkgs, system, ... }:
+      perSystem =
+        {
+          config,
+          pkgs,
+          system,
+          ...
+        }:
         let
           name = "vault-plugin-secrets-github";
           package = "github.com/martinbaillie/${name}";
@@ -47,50 +54,56 @@
           date = self.lastModifiedDate or "19700101";
           go = pkgs.go_1_22;
         in
-        rec
         {
           _module.args.pkgs = import nixpkgs {
             inherit system;
             config.allowUnfree = true; # BSL2... Hashicorp...
-            overlays = [ devshell.overlays.default gomod2nix.overlays.default ];
+            overlays = [
+              devshell.overlays.default
+              gomod2nix.overlays.default
+            ];
           };
 
-          packages.default =
-            gomod2nix.legacyPackages.${system}.buildGoApplication {
-              inherit name go;
-              src = gitignore.lib.gitignoreSource ./.;
-              # Must be added due to bug:
-              # https://github.com/nix-community/gomod2nix/issues/120
-              pwd = ./.;
-              flags = [ "-trimpath" ];
-              ldflags = [
-                "-s"
-                "-w"
-                "-extld ld"
-                "-extldflags"
-                "-static"
-                "-X ${package}/github.projectName=${name}"
-                "-X ${package}/github.projectDocs=https://${package}"
-                "-X github.com/prometheus/common/version.BuildDate=${date}"
-                "-X github.com/prometheus/common/version.Revision=${rev}"
-                "-X github.com/prometheus/common/version.Version=${ver}"
-                # TODO: Pass in from CI.
-                "-X github.com/prometheus/common/version.Branch=main"
-                "-X github.com/prometheus/common/version.BuildUser=nix"
-              ];
-              doCheck = false;
-            };
+          packages.default = gomod2nix.legacyPackages.${system}.buildGoApplication {
+            inherit name go;
+            src = gitignore.lib.gitignoreSource ./.;
+            # Must be added due to bug:
+            # https://github.com/nix-community/gomod2nix/issues/120
+            pwd = ./.;
+            modules = ./gomod2nix.toml;
+            flags = [ "-trimpath" ];
+            # CGO_ENABLED = 0;
+            ldflags = [
+              "-s"
+              "-w"
+              "-extld ld"
+              "-extldflags"
+              "-static"
+              "-X ${package}/github.projectName=${name}"
+              "-X ${package}/github.projectDocs=https://${package}"
+              "-X github.com/prometheus/common/version.BuildDate=${date}"
+              "-X github.com/prometheus/common/version.Revision=${rev}"
+              "-X github.com/prometheus/common/version.Version=${ver}"
+              # TODO: Pass in from CI.
+              "-X github.com/prometheus/common/version.Branch=main"
+              "-X github.com/prometheus/common/version.BuildUser=nix"
+            ];
+            doCheck = false;
+          };
 
           devShells.default = pkgs.devshell.mkShell rec {
             inherit name;
 
             motd = builtins.concatStringsSep "\n" [
-              "{2}${ name}{reset}"
+              "{2}${name}{reset}"
               "menu                              - available commands"
             ];
 
             env = [
-              { name = "VAULT_ADDR"; value = "http://127.0.0.1:8200"; }
+              {
+                name = "VAULT_ADDR";
+                value = "http://127.0.0.1:8200";
+              }
             ];
 
             packages = with pkgs; [
@@ -105,122 +118,140 @@
               vault-bin
             ];
 
-            commands = with pkgs; let prjRoot = "cd $PRJ_ROOT;"; in
-            [
-              {
-                inherit name;
-                command = "nix run";
-                help = "build and run the project binary";
-              }
-              {
-                name = "build";
-                command = "nix build";
-                help = "build and run the project binary";
-              }
-              {
-                name = "todo";
-                command = prjRoot + ''
-                  ${gnugrep}/bin/grep --exclude=flake.nix \
-                    --exclude-dir=.direnv --color=auto --text \
-                    -InRo -E ' TODO.*' .
-                '';
-                help = "show project TODO items";
-              }
-              {
-                name = "clean";
-                command = prjRoot + ''
-                  echo >&2 "==> Cleaning"
-                  rm -rf test result
-                '';
-                help = "clean transient files";
-              }
-              {
-                name = "tidy";
-                command = prjRoot + ''
-                  echo >&2 "==> Tidying modules"
-                  go mod tidy && gomod2nix
-                '';
-                help = "clean transient files";
-              }
-              {
-                name = "lint";
-                command = prjRoot + ''
-                  echo >&2 "==> Linting"
-                  if [ -v CI ]; then
-                    mkdir -p test
-                    ${golangci-lint}/bin/golangci-lint run \
-                        --out-format=checkstyle | tee test/checkstyle.xml
-                  else
-                    ${golangci-lint}/bin/golangci-lint run --fast
-                  fi
-                '';
-                help = "lint the project (heavyweight when CI=true)";
-              }
-              {
-                name = "unit";
-                command = prjRoot + ''
-                  [[ $# -eq 0 ]] && set -- "./..."
-                  echo >&2 "==> Unit testing"
-                  [ -v DEBUG ] && fmt=standard-verbose || fmt=short-verbose
-                  mkdir -p test
-                  if [ -v CI ]; then
-                    ${gotestsum}/bin/gotestsum \
-                        --format $fmt --junitfile test/junit.xml -- -race \
-                        -coverprofile=test/coverage.out -covermode=atomic $@
-                  else
-                    ${gotestsum}/bin/gotestsum \
-                        --format $fmt --junitfile test/junit.xml -- $@
-                  fi
-                '';
-                help = "unit test the project";
-              }
-              {
-                name = "integration-server";
-                command = prjRoot + ''
-                  echo >&2 "==> Integration server"
-                  [ -v DEBUG ] && lvl=trace || lvl=error
-                  [ ! -f "result/bin/vault-plugin-secrets-github" ] && build
-                  pkill vault && sleep 2 || true
-                  rm -f test/vault.pid
-                  (
-                  trap 'rm -f test/vault.pid' EXIT
-                  ${vault-bin}/bin/vault server \
-                      -dev \
-                      -dev-plugin-dir=$(${coreutils}/bin/realpath result/bin) \
-                      -dev-root-token-id=root \
-                      -log-level=$lvl
-                  ) &
-                  echo $! > test/vault.pid
-                  sleep 2
-                  ${vault-bin}/bin/vault write sys/plugins/catalog/${name} \
-                      sha_256=$(${coreutils}/bin/sha256sum result/bin/${name} |
-                                cut -d' ' -f1) command=${name}
-                  ${vault-bin}/bin/vault secrets enable \
-                      -path=github -plugin-name=${name} plugin
-                '';
-                help = "run a background Vault with the plugin enabled";
-              }
-              {
-                name = "integration";
-                # To run integration tests against a real GitHub App
-                # installation:
-                # $ env \
-                #   BASE_URL=https://api.github.com \
-                # 	APP_ID=<your application id> \
-                #	  ORG_NAME=<org_name> \
-                #	  INSTALLATION_ID=<installation_id> \
-                # 	PRV_KEY="$(cat /path/to/your/app/prv_key_file)" integration
-                #
-                # NOTE: this will automatically skip racyness tests to avoid
-                # GitHub API rate limiting.
-                command = prjRoot + ''
-                  echo >&2 "==> Integration testing"
-                  [ ! -f "test/vault.pid" ] && integration-server
-                  unit -count 1 -tags integration ./...
-                '';
-                help = "unit and integration test the project";
-              }
-            ];
+            commands =
+              with pkgs;
+              let
+                prjRoot = "cd $PRJ_ROOT;";
+              in
+              [
+                {
+                  inherit name;
+                  command = "nix run";
+                  help = "build and run the project binary";
+                }
+                {
+                  name = "build";
+                  command = "nix build";
+                  help = "build and run the project binary";
+                }
+                {
+                  name = "todo";
+                  command =
+                    prjRoot
+                    + ''
+                      ${gnugrep}/bin/grep --exclude=flake.nix \
+                        --exclude-dir=.direnv --color=auto --text \
+                        -InRo -E ' TODO.*' .
+                    '';
+                  help = "show project TODO items";
+                }
+                {
+                  name = "clean";
+                  command =
+                    prjRoot
+                    + ''
+                      echo >&2 "==> Cleaning"
+                      rm -rf test result
+                    '';
+                  help = "clean transient files";
+                }
+                {
+                  name = "tidy";
+                  command =
+                    prjRoot
+                    + ''
+                      echo >&2 "==> Tidying modules"
+                      go mod tidy && gomod2nix
+                    '';
+                  help = "clean transient files";
+                }
+                {
+                  name = "lint";
+                  command =
+                    prjRoot
+                    + ''
+                      echo >&2 "==> Linting"
+                      if [ -v CI ]; then
+                        mkdir -p test
+                        ${golangci-lint}/bin/golangci-lint run \
+                            --out-format=checkstyle | tee test/checkstyle.xml
+                      else
+                        ${golangci-lint}/bin/golangci-lint run --fast
+                      fi
+                    '';
+                  help = "lint the project (heavyweight when CI=true)";
+                }
+                {
+                  name = "unit";
+                  command =
+                    prjRoot
+                    + ''
+                      [[ $# -eq 0 ]] && set -- "./..."
+                      echo >&2 "==> Unit testing"
+                      [ -v DEBUG ] && fmt=standard-verbose || fmt=short-verbose
+                      mkdir -p test
+                      if [ -v CI ]; then
+                        ${gotestsum}/bin/gotestsum \
+                            --format $fmt --junitfile test/junit.xml -- -race \
+                            -coverprofile=test/coverage.out -covermode=atomic $@
+                      else
+                        ${gotestsum}/bin/gotestsum \
+                            --format $fmt --junitfile test/junit.xml -- $@
+                      fi
+                    '';
+                  help = "unit test the project";
+                }
+                {
+                  name = "integration-server";
+                  command =
+                    prjRoot
+                    + ''
+                      echo >&2 "==> Integration server"
+                      [ -v DEBUG ] && lvl=trace || lvl=error
+                      [ ! -f "result/bin/vault-plugin-secrets-github" ] && build
+                      pkill vault && sleep 2 || true
+                      rm -f test/vault.pid
+                      (
+                      trap 'rm -f test/vault.pid' EXIT
+                      ${vault-bin}/bin/vault server \
+                          -dev \
+                          -dev-plugin-dir=$(${coreutils}/bin/realpath result/bin) \
+                          -dev-root-token-id=root \
+                          -log-level=$lvl
+                      ) &
+                      echo $! > test/vault.pid
+                      sleep 2
+                      ${vault-bin}/bin/vault write sys/plugins/catalog/${name} \
+                          sha_256=$(${coreutils}/bin/sha256sum result/bin/${name} |
+                                    cut -d' ' -f1) command=${name}
+                      ${vault-bin}/bin/vault secrets enable \
+                          -path=github -plugin-name=${name} plugin
+                    '';
+                  help = "run a background Vault with the plugin enabled";
+                }
+                {
+                  name = "integration";
+                  # To run integration tests against a real GitHub App
+                  # installation:
+                  # $ env \
+                  #   BASE_URL=https://api.github.com \
+                  # 	APP_ID=<your application id> \
+                  #	  ORG_NAME=<org_name> \
+                  #	  INSTALLATION_ID=<installation_id> \
+                  # 	PRV_KEY="$(cat /path/to/your/app/prv_key_file)" integration
+                  #
+                  # NOTE: this will automatically skip racyness tests to avoid
+                  # GitHub API rate limiting.
+                  command =
+                    prjRoot
+                    + ''
+                      echo >&2 "==> Integration testing"
+                      [ ! -f "test/vault.pid" ] && integration-server
+                      unit -count 1 -tags integration ./...
+                    '';
+                  help = "unit and integration test the project";
+                }
+              ];
           };
         };
     });
--- go.mod
+++ go.mod
@@ -1,87 +1,100 @@
 module github.com/martinbaillie/vault-plugin-secrets-github
 
-go 1.22.0
+go 1.23.4
 
 require (
 	github.com/bradleyfalzon/ghinstallation v1.1.1
-	github.com/hashicorp/go-hclog v1.6.2
-	github.com/hashicorp/vault/api v1.12.1
-	github.com/hashicorp/vault/sdk v0.11.1
-	github.com/prometheus/client_golang v1.19.0
-	github.com/prometheus/common v0.50.0
+	github.com/hashicorp/go-hclog v1.6.3
+	github.com/hashicorp/vault/api v1.15.0
+	github.com/hashicorp/vault/sdk v0.14.1
+	github.com/prometheus/client_golang v1.20.5
+	github.com/prometheus/common v0.62.0
 	gotest.tools v2.2.0+incompatible
 )
 
 require (
-	github.com/Microsoft/go-winio v0.6.1 // indirect
+	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/armon/go-metrics v0.4.1 // indirect
 	github.com/armon/go-radix v1.0.0 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
-	github.com/cenkalti/backoff/v3 v3.2.2 // indirect
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/cenkalti/backoff/v4 v4.3.0 // indirect
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
-	github.com/docker/distribution v2.8.2+incompatible // indirect
-	github.com/docker/docker v24.0.7+incompatible // indirect
-	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/distribution/reference v0.6.0 // indirect
+	github.com/docker/docker v27.5.0+incompatible // indirect
+	github.com/docker/go-connections v0.5.0 // indirect
 	github.com/docker/go-units v0.5.0 // indirect
-	github.com/evanphx/json-patch/v5 v5.6.0 // indirect
-	github.com/fatih/color v1.16.0 // indirect
+	github.com/evanphx/json-patch/v5 v5.9.0 // indirect
+	github.com/fatih/color v1.18.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
 	github.com/frankban/quicktest v1.14.3 // indirect
-	github.com/go-jose/go-jose/v3 v3.0.1 // indirect
+	github.com/go-jose/go-jose/v4 v4.0.4 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
 	github.com/google/go-github/v29 v29.0.3 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.25.1 // indirect
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-cleanhttp v0.5.2 // indirect
+	github.com/hashicorp/go-hmac-drbg v0.0.0-20210916214228-a6e5a68489f6 // indirect
 	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
-	github.com/hashicorp/go-kms-wrapping/entropy/v2 v2.0.0 // indirect
-	github.com/hashicorp/go-kms-wrapping/v2 v2.0.8 // indirect
+	github.com/hashicorp/go-kms-wrapping/entropy/v2 v2.0.1 // indirect
+	github.com/hashicorp/go-kms-wrapping/v2 v2.0.16 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
-	github.com/hashicorp/go-plugin v1.6.0 // indirect
-	github.com/hashicorp/go-retryablehttp v0.7.1 // indirect
+	github.com/hashicorp/go-plugin v1.6.2 // indirect
+	github.com/hashicorp/go-retryablehttp v0.7.7 // indirect
 	github.com/hashicorp/go-rootcerts v1.0.2 // indirect
-	github.com/hashicorp/go-secure-stdlib/mlock v0.1.2 // indirect
-	github.com/hashicorp/go-secure-stdlib/parseutil v0.1.7 // indirect
-	github.com/hashicorp/go-secure-stdlib/plugincontainer v0.3.0 // indirect
+	github.com/hashicorp/go-secure-stdlib/cryptoutil v0.1.0 // indirect
+	github.com/hashicorp/go-secure-stdlib/mlock v0.1.3 // indirect
+	github.com/hashicorp/go-secure-stdlib/parseutil v0.1.9 // indirect
+	github.com/hashicorp/go-secure-stdlib/plugincontainer v0.4.1 // indirect
 	github.com/hashicorp/go-secure-stdlib/strutil v0.1.2 // indirect
-	github.com/hashicorp/go-sockaddr v1.0.2 // indirect
+	github.com/hashicorp/go-sockaddr v1.0.7 // indirect
 	github.com/hashicorp/go-uuid v1.0.3 // indirect
-	github.com/hashicorp/go-version v1.6.0 // indirect
-	github.com/hashicorp/golang-lru v0.5.4 // indirect
-	github.com/hashicorp/hcl v1.0.1-vault-5 // indirect
-	github.com/hashicorp/yamux v0.1.1 // indirect
+	github.com/hashicorp/go-version v1.7.0 // indirect
+	github.com/hashicorp/golang-lru v1.0.2 // indirect
+	github.com/hashicorp/hcl v1.0.1-vault-7 // indirect
+	github.com/hashicorp/yamux v0.1.2 // indirect
 	github.com/joshlf/go-acl v0.0.0-20200411065538-eae00ae38531 // indirect
-	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mitchellh/copystructure v1.2.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
-	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/mitchellh/reflectwalk v1.0.2 // indirect
+	github.com/moby/docker-image-spec v1.3.1 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/oklog/run v1.1.0 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
-	github.com/opencontainers/image-spec v1.1.0-rc2.0.20221005185240-3a7f492d3f1b // indirect
+	github.com/opencontainers/image-spec v1.1.0 // indirect
+	github.com/petermattis/goid v0.0.0-20241211131331-93ee7e083c43 // indirect
 	github.com/pierrec/lz4 v2.6.1+incompatible // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
-	github.com/prometheus/client_model v0.6.0 // indirect
-	github.com/prometheus/procfs v0.12.0 // indirect
+	github.com/prometheus/client_model v0.6.1 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/ryanuber/go-glob v1.0.0 // indirect
-	github.com/stretchr/testify v1.8.4 // indirect
-	go.uber.org/atomic v1.10.0 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
-	golang.org/x/mod v0.11.0 // indirect
-	golang.org/x/net v0.22.0 // indirect
-	golang.org/x/sys v0.18.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	golang.org/x/time v0.3.0 // indirect
-	golang.org/x/tools v0.10.0 // indirect
-	google.golang.org/genproto/googleapis/rpc v0.0.0-20231030173426-d783a09b4405 // indirect
-	google.golang.org/grpc v1.60.1 // indirect
-	google.golang.org/protobuf v1.33.0 // indirect
+	github.com/sasha-s/go-deadlock v0.3.5 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.59.0 // indirect
+	go.opentelemetry.io/otel v1.34.0 // indirect
+	go.opentelemetry.io/otel/metric v1.34.0 // indirect
+	go.opentelemetry.io/otel/trace v1.34.0 // indirect
+	go.uber.org/atomic v1.11.0 // indirect
+	golang.org/x/crypto v0.32.0 // indirect
+	golang.org/x/net v0.34.0 // indirect
+	golang.org/x/sys v0.29.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	golang.org/x/time v0.9.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250115164207-1a7da9e5054f // indirect
+	google.golang.org/grpc v1.69.4 // indirect
+	google.golang.org/protobuf v1.36.3 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
